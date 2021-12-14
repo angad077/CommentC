7b42489d5421c1bd01245e29fa0c7540309a102b
angad <angadreact01@gmail.com>
2021-12-14 07:34:43 -0800

    first commit

---
+++ comment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // --> single line comment.Comment shortcut{ctrl + /} they hide all your comment or message
+    /* --> multi line comment /* --> {that is a start point}
+    my name is angad 
+    hello world
+    I learn c language
+    bye world  {that is a end point of } --> */
+    printf(" hello world");
+    return 0;
+}
